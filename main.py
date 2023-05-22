0e77fa41f2824f6bb4c02181450794c6abdfacb9
Giovanni Martinovsky <104166852+GioErnst@users.noreply.github.com>
2023-05-22 14:51:13 -0400

    fix: alterando o valor inicial de duas variaveis

---
+++ main.py
@@ -0,0 +1,17 @@
+# Definindo as variáveis
+a = 4
+b = 3
+c = 2
+d = 5
+
+# Multiplicando as variáveis
+res_mult = a * b * c * d
+res_add = a + b + c + d
+res_div = a/b/c/d
+media = a + b + c + d / 4
+
+# Imprimindo o resultado
+print(res_mult)
+print(res_add)
+print(res_div)
+print(media)
